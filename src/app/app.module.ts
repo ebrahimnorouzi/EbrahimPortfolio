ea4e38cc404c116490b069f6a7c5d9906d0c0584
Ebrahim <ebrahimnorouzi@Ebrahims-MBP.fritz.box>
2022-05-30 15:07:33 +0200

    portfolio repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProjectsListComponent } from './components/projects-list/projects-list.component';
+import { SkillsListComponent } from './components/skills-list/skills-list.component';
+import { ToolsListComponent } from './components/tools-list/tools-list.component';
+import { PortfolioPageComponent } from './pages/portfolio-page/portfolio-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    ContactFormComponent,
+    HeaderComponent,
+    ProjectsListComponent,
+    SkillsListComponent,
+    ToolsListComponent,
+    PortfolioPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBoKTb7hPLWAv9SMyY5grR6v2-HyDHqLUw",
+    authDomain: "angularportfolio-bb394.firebaseapp.com",
+    projectId: "angularportfolio-bb394",
+    storageBucket: "angularportfolio-bb394.appspot.com",
+    messagingSenderId: "910762891442",
+    appId: "1:910762891442:web:ccf1d03d001aa18e5451ab",
+    measurementId: "G-SZY6LQWNY0"
+  }
 };
